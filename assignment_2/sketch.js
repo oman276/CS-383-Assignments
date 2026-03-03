577c2584161b4a0f8f0a8538c443be99dcf8840a
oman276 <owengallagher276@gmail.com>
2026-03-02 23:16:26 -0500

    test base file for assignment change

---
+++ assignment_2/sketch.js
@@ -0,0 +1,14 @@
+function preload() {}
+
+function setup() {}
+
+function draw() {
+  background(20);
+  textSize(16);
+  fill(255);
+  text("PLEASE STAND BY...", -width / 2 + 20, -height / 2 + 30);
+}
+
+function windowResized() {}
+
+function paramChanged(name) {}
