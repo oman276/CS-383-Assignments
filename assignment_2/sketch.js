2530f946596d2d2a1f2ca5ad17cd565a890c7e05
oman276 <owengallagher276@gmail.com>
2026-03-02 23:57:05 -0500

    WIP speech rec using p5 speech

--- assignment_2/sketch.js
+++ assignment_2/sketch.js
@@ -1,14 +1,32 @@
 function preload() {}
 
-function setup() {}
+let speechRec;
+let audiotext;
+
+function setup() {
+  speechRec = new p5.SpeechRec("en-US", gotSpeech);
+  audiotext = "";
+
+  speechRec.start(true, false);
+}
 
 function draw() {
-  background(20);
-  textSize(16);
+  createCanvas(400, 400);
+  background(40);
+  textSize(30);
   fill(255);
-  text("PLEASE STAND BY...", -width / 2 + 20, -height / 2 + 30);
+  textColor = color(255, 0, 0);
+  text(audiotext, 50, 50);
 }
 
 function windowResized() {}
 
 function paramChanged(name) {}
+
+function gotSpeech() {
+  if (speechRec.resultValue) {
+    console.log(speechRec.resultString);
+    audiotext = speechRec.resultString;
+    // text(speechRec.resultString, 50, 100);
+  }
+}
