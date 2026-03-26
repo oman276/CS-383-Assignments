6a684b51bc7b613525f7f6f04d41017fcd280af7
Owen Gallagher <owengallagher276@gmail.com>
2026-03-26 16:11:22 -0400

    change primary interaction mode to mouse action

--- assignment_3/teamwork-ml/scripts/networking/python_send_mouse.gd
+++ assignment_3/teamwork-ml/scripts/networking/python_send_mouse.gd
@@ -4,8 +4,39 @@ extends Node2D
 var timeSinceLastSend: float = 0.0
 # delta of sending updates per second
 @onready var sendDelta: float = 1.0 / sendPerSecond
-@onready var player = get_parent()
+
+var hasStartPosition: bool = false
+var lastMousePosition: Vector2 = Vector2.ZERO
+
 
 func _process(delta: float) -> void:
-    
-    pass
+	timeSinceLastSend += delta
+
+	if Input.is_action_just_released("mouse"):
+		print("mouse up")
+		hasStartPosition = false
+
+	elif Input.is_action_just_pressed("mouse"):
+		print("mouse down")
+		# start recording mouse positions
+		hasStartPosition = true
+		lastMousePosition = get_global_mouse_position()
+
+	elif hasStartPosition:
+		var currentMousePosition = get_global_mouse_position()
+		var direction = currentMousePosition - lastMousePosition
+		if direction != Vector2.ZERO:
+			print("sending velocity update to Python server: %s" % direction)
+
+			var message = "update,%f,%f,%f,%f" % [
+				global_position.x,
+				global_position.y,
+				direction.x,
+				direction.y
+			]
+			var udp = PacketPeerUDP.new()
+			udp.set_dest_address("127.0.0.1", 5005)
+			udp.put_packet(message.to_utf8_buffer())
+			udp.close()
+
+		lastMousePosition = currentMousePosition
